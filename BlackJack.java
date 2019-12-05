306a82303fba6f151573b2d6d3d741a2d4844b77
AyushT02 <53292604+AyushT02@users.noreply.github.com>
2019-12-04 19:36:20 -0800

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -14,7 +14,27 @@ public class BlackJack {
             player.draw(d.dealCard());
             dealer.draw(d.dealCard());
             System.out.println(player.toString() + " ");
-
+            System.out.println("Player Hand: " + player.toString() + " ");
+            System.out.println("Dealer Hand: " + dealer.toString() + " ");
+            player.totalValue();
+            System.out.println(player.totalValue());
+            dealer.totalValue();
+            System.out.println(dealer.totalValue());
+            if(player.checkAce() == true) {
+                if(player.totalValue() > 21) {
+                    aceVar = player.totalValue() - 10;
+            } else {
+                    aceVar = player.totalValue();
+            }
+            if(player.totalValue() == 21) {
+               System.out.println("You win!");   
+            }
+            if(dealer.totalValue() <= 17) {
+               dealer.draw(d.dealCard());   
+            }
+            if(player.totalValue() > 21) {
+               System.out.println("You lose!");   
+            }
         }
         while(!hitOrStand.equalsIgnoreCase("hit")) {
             System.out.println("Please say hit or stand");
@@ -23,6 +43,22 @@ public class BlackJack {
             if(h.equalsIgnoreCase("hit")) {
                 break;
             }
+            if(player.checkAce() == true) {
+                if(player.totalValue() > 21) {
+                    aceVar = player.totalValue() - 10;
+            } else {
+                    aceVar = player.totalValue();
+            }
+            if(player.totalValue() == 21) {
+               System.out.println("You win!");   
+            }
+            if(dealer.totalValue() <= 17) {
+               dealer.draw(d.dealCard());   
+            }
+            if(player.totalValue() > 21) {
+               System.out.println("You lose!");   
+            }
+        }
         }
         while(hitOrStand.equalsIgnoreCase("hit")) {
             System.out.println("Would you like to hit or stand?");
@@ -34,7 +70,27 @@ public class BlackJack {
                 dealer.draw(d.dealCard());
                 System.out.println("Player Hand: " + player.toString() + " ");
                 System.out.println("Dealer Hand: " + dealer.toString() + " ");
+                player.totalValue();
+                System.out.println(player.totalValue());
+                dealer.totalValue();
+                System.out.println(dealer.totalValue());
             }
+            if(player.checkAce() == true) {
+                if(player.totalValue() > 21) {
+                    aceVar = player.totalValue() - 10;
+            } else {
+                    aceVar = player.totalValue();
+            }
+            if(player.totalValue() == 21) {
+               System.out.println("You win!");   
+            }
+            if(dealer.totalValue() <= 17) {
+               dealer.draw(d.dealCard());   
+            }
+            if(player.totalValue() > 21) {
+               System.out.println("You lose!");   
+            }
+        }
         }
         if(player.totalValue() == 21) {
             System.out.println("You win!");
